872a3cfffc7afa9e75fe80c5431e6b47e9924dc7
Viraj <virajphalke968@gmail.com>
2026-01-06 21:25:50 -0800

    code updated

---
+++ emp.java
@@ -0,0 +1,22 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import in.ashokit.config.AppConfig;
+import in.ashokit.service.UserService;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+
+		// Starting IOC Container
+		ApplicationContext ctxt = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		// get bean obj from ioc
+		UserService bean = ctxt.getBean(UserService.class);
+
+		// call bean method
+		bean.printName();
+                 int i=0;
+
+	}
+}
